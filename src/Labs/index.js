e8c74c1a94f07301e8051298f80c482e9c27c91d
Adam Koulopoulos <adam@Adams-MacBook-Pro.local>
2023-11-21 17:33:11 -0500

    fix naming

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,17 +1,23 @@
 import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Assignment3 from "./a3";
+import Assignment4 from "./a4";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function Labs() {
   return (
-    <div className="container">
-      <h1>Labs</h1>
-      <Link to="a3">Assignment 3</Link>
-      <Routes>
-        <Route path="/" element={<Navigate to="a3" />} />
-        {/* <Route path="/" element={<Assignment3 />} /> */}
-        <Route path="a3" element={<Assignment3 />} />
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <div className="container">
+        <h1>Labs</h1>
+        <Link to="a3">Assignment 3</Link>
+        <Routes>
+          <Route path="/" element={<Navigate to="a4" />} />
+          {/* <Route path="/" element={<Assignment3 />} /> */}
+          <Route path="a3" element={<Assignment3 />} />
+          <Route path="a4" element={<Assignment4 />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/Labs/store/index.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import helloReducer from "../a4/ReduxExamples/HelloRedux/helloReducer";
+import counter1Reducer from "../a4/ReduxExamples/counter1Reducer";
+import counter2Reducer from "../a4/ReduxExamples/counter2Reducer";
+import addReducer from "../a4/ReduxExamples/AddRedux/addReducer";
+import todosReducer from "../a4/ReduxExamples/todos/todosReducer";
+
+const store = configureStore({
+  reducer: {
+    helloReducer,
+    counter1Reducer,
+    counter2Reducer,
+    addReducer,
+    todosReducer,
+  },
+});
+export default store;
