ae5b810ddf6a029fc779bd128f469b552e355ea5
Adam Koulopoulos <adam@Adams-MacBook-Pro.local>
2023-11-21 17:43:02 -0500

    .

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,15 +1,18 @@
 import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Assignment3 from "./a3";
+import Assignment5 from "./a5";
 
 function Labs() {
   return (
     <div className="container">
       <h1>Labs</h1>
       <Link to="a3">Assignment 3</Link>
+      <Link to="a5">Assignment 5</Link>
       <Routes>
         <Route path="/" element={<Navigate to="a3" />} />
         {/* <Route path="/" element={<Assignment3 />} /> */}
         <Route path="a3" element={<Assignment3 />} />
+        <Route path="a5" element={<Assignment5 />} />
       </Routes>
     </div>
   );
